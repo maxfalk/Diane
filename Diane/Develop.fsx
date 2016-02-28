16eca3d9026c922c1b716da28acd67f31e4d2a8f
Max <max.j.falk@gmail.com>
2016-02-28 18:57:13 +0100

    test

---
+++ Diane/Develop.fsx
@@ -0,0 +1,13 @@
+﻿
+#r "../packages/Fsharp.Data.2.2.5/lib/net40/Fsharp.Data.dll"
+open System.Net
+open FSharp.Data
+let address = "https://api.telegram.org/bot"
+let token = "188654328:AAEUweZorPX1FjIKwX3xjlwcXICGB40hksQ"
+let useMethod = "getUpdates"
+let sep = "/"
+let client = new WebClient();
+let data = client.DownloadString(address + token + sep + useMethod)
+
+type updateMessage = JsonProvider<data>
+let json = updateMessage.
